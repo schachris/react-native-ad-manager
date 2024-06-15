8562621ccfa7f5fef6140cd4074d1706be803218
Christian Schaffrath <christian.schaffrath@crestalize.com>
2024-06-15 16:10:03 +0200

    fix(newarch): make it work with new arch

---
+++ android/src/newarch/AdmanagerMobileAdsConsentSpec.java
@@ -0,0 +1,9 @@
+package com.admanagermobileads;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+
+abstract class AdmanagerMobileAdsConsentSpec extends NativeAdmanagerMobileAdsConsentSpec {
+  AdmanagerMobileAdsConsentSpec(ReactApplicationContext context) {
+    super(context);
+  }
+}
